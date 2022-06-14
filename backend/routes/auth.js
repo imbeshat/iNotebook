92d85bc653a45eb9b2f0ff5e4f0256142fd2be1c
Imbeshat <md.imbeshat@gmail.com>
2022-06-14 15:24:29 +0530

    Login end point and auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -47,7 +47,45 @@ router.post(
 			// res.json(user);
 		} catch (error) {
 			console.error(error.message);
-			res.status(500).send('Some Error Occured');
+			res.status(500).send('Internal Server Error');
+		}
+	}
+);
+
+//  Create a User using: POST "/api/auth/login". No login required
+router.post(
+	'/login',
+	[body('email', 'Enter a valid email').isEmail(), body('password', 'Password lcan not br blank').exists()],
+	async (req, res) => {
+		//  If there are errors, return bad request and the errors
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			// Check wheter the user with this email exists already
+			let user = await User.findOne({ email });
+			if (!user) {
+				return res.status(400).json({ error: 'Incorrect Credentials' });
+			}
+			const passwordCompare = await bcrypt.compare(password, user.password);
+			if (!passwordCompare) {
+				return res.status(400).json({ error: 'Incorrect Credentials' });
+			}
+
+			const data = {
+				user: {
+					id: user.id,
+				},
+			};
+			const authToken = jwt.sign(data, JWT_SECRET);
+			res.json({ authToken });
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send('Internal Server Error');
 		}
 	}
 );
