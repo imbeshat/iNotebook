7bc8610a6942d58a4212544c368cdeddeb9f73b6
Imbeshat <md.imbeshat@gmail.com>
2022-06-15 23:16:55 +0530

    Setting Navbar and Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import { About } from './components/About';
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>iNoteBook</h1>
-		</div>
+		<>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path='/' exact element={<Home />} />
+					<Route path='/about' exact element={<About />} />
+					{/* <Route path='/second' exact element={<Second />} /> */}
+				</Routes>
+			</Router>
+		</>
 	);
 }
 
