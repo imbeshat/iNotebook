b8d99b8d53e9fbc5da3fa4645498e41b421f65c1
Imbeshat <md.imbeshat@gmail.com>
2022-06-15 12:46:30 +0530

    Endpoint for updating a note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Note = require('../models/Note');
 const { body, validationResult } = require('express-validator');
 
-//  ROUTE 1: Get All the notes using: GET "/api/auth/fetchallnotes". Login required
+//  ROUTE 1: Get All the notes using: GET "/api/notes/fetchallnotes". Login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
 	try {
 		const notes = await Note.find({ user: req.user.id });
@@ -15,7 +15,7 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 	}
 });
 
-//  ROUTE 2: Add a new Note using: GET "/api/auth/addnote". Login required
+//  ROUTE 2: Add a new Note using: GET "/api/notes/addnote". Login required
 router.post(
 	'/addnote',
 	fetchuser,
@@ -33,6 +33,7 @@ router.post(
 				return res.status(400).json({ errors: errors.array() });
 			}
 
+			//  Create new Note object
 			const note = new Note({
 				title,
 				description,
@@ -47,5 +48,47 @@ router.post(
 		}
 	}
 );
+
+//  ROUTE 3: Update an existing Note using: GET "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+	try {
+		const { title, description, tag } = req.body;
+
+		//  If there are errors, return bad request and the errors
+		// const errors = validationResult(req);
+		// if (!errors.isEmpty()) {
+		// 	return res.status(400).json({ errors: errors.array() });
+		// }
+
+		//  Create new Note object
+		const newNote = {};
+
+		if (title) {
+			newNote.title = title;
+		}
+		if (description) {
+			newNote.description = description;
+		}
+		if (tag) {
+			newNote.tag = tag;
+		}
+
+		//  Find the note to be updated
+		let note = await Note.findById(req.params.id);
+		if (!note) {
+			return res.status(404).send('Not Found');
+		}
+		if (note.user.toString() !== req.user.id) {
+			return res.status(401).send('Unauthorised Access');
+		}
+
+		note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+
+		res.json(note);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Internal Server Error');
+	}
+});
 
 module.exports = router;
