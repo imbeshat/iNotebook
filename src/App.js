5e2a46a368e3fd6953118f724abb39a56afe829f
Imbeshat <md.imbeshat@gmail.com>
2022-06-16 15:11:50 +0530

    Modified File

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { Home } from './components/Home';
 import { About } from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
 	return (
 		<>
-			<Router>
-				<Navbar />
-				<Routes>
-					<Route path='/' exact element={<Home />} />
-					<Route path='/about' exact element={<About />} />
-					{/* <Route path='/second' exact element={<Second />} /> */}
-				</Routes>
-			</Router>
+			<NoteState>
+				<Router>
+					<Navbar />
+					<div className='container'>
+						<Routes>
+							<Route path='/' exact element={<Home />} />
+							<Route path='/about' exact element={<About />} />
+							{/* <Route path='/second' exact element={<Second />} /> */}
+						</Routes>
+					</div>
+				</Router>
+			</NoteState>
 		</>
 	);
 }
