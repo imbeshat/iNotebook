6af42b5e53657af0140f3c32214a7257a95ca418
Imbeshat <md.imbeshat@gmail.com>
2022-06-16 21:50:08 +0530

    Adding note item in a Separate Note Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import noteContext from '../context/notes/noteContext';
 
 export const Home = () => {
+	const context = useContext(noteContext);
+	const { notes, setNotes } = context;
 	return (
 		<div>
 			<div className='container my-3'>
 				<h1>Add a Note</h1>
 				<form>
 					<div className='mb-3'>
-						<label for='exampleInputEmail1' className='form-label'>
+						<label htmlFor='exampleInputEmail1' className='form-label'>
 							Email address
 						</label>
 						<input
@@ -21,14 +24,14 @@ export const Home = () => {
 						</div>
 					</div>
 					<div className='mb-3'>
-						<label for='exampleInputPassword1' className='form-label'>
+						<label htmlFor='exampleInputPassword1' className='form-label'>
 							Password
 						</label>
 						<input type='password' className='form-control' id='exampleInputPassword1' />
 					</div>
 					<div className='mb-3 form-check'>
 						<input type='checkbox' className='form-check-input' id='exampleCheck1' />
-						<label className='form-check-label' for='exampleCheck1'>
+						<label className='form-check-label' htmlFor='exampleCheck1'>
 							Check me out
 						</label>
 					</div>
@@ -38,6 +41,9 @@ export const Home = () => {
 				</form>
 				<div className='container my-3'>
 					<h1>Your Notes</h1>
+					{notes.map((note) => {
+						return note.title;
+					})}
 				</div>
 			</div>
 		</div>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,28 @@ import { useState } from 'react';
 import NoteContext from './noteContext';
 
 const NoteState = (props) => {
-	return <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>;
+	const notesInitial = [
+		{
+			_id: '62a9772a76539aa43d8dabc0',
+			user: '62a84bdc8097b4c47d521962',
+			title: 'Marvels',
+			description: 'Superheroes of marvel universe',
+			tag: 'Superhero',
+			date: '2022-06-15T06:07:38.869Z',
+			__v: 0,
+		},
+		{
+			_id: '62aa0dcf331195387210018d',
+			user: '62a84bdc8097b4c47d521962',
+			title: 'DC Super',
+			description: 'Superheroes of DC universe',
+			tag: 'Superhero',
+			date: '2022-06-15T16:50:23.836Z',
+			__v: 0,
+		},
+	];
+	const [notes, setNotes] = useState(notesInitial);
+	return <NoteContext.Provider value={{ notes, setNotes }}>{props.children}</NoteContext.Provider>;
 };
 
 export default NoteState;
