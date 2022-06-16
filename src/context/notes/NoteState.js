63638525b5107bbdd013934f04a5783cfddaa967
Imbeshat <md.imbeshat@gmail.com>
2022-06-16 22:18:41 +0530

    Adding NoteItem in a separate Note component

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -21,6 +21,33 @@ const NoteState = (props) => {
 			date: '2022-06-15T16:50:23.836Z',
 			__v: 0,
 		},
+		{
+			_id: '62aa0dcf331195387210018d',
+			user: '62a84bdc8097b4c47d521962',
+			title: 'DC Super',
+			description: 'Superheroes of DC universe',
+			tag: 'Superhero',
+			date: '2022-06-15T16:50:23.836Z',
+			__v: 0,
+		},
+		{
+			_id: '62aa0dcf331195387210018d',
+			user: '62a84bdc8097b4c47d521962',
+			title: 'DC Super',
+			description: 'Superheroes of DC universe',
+			tag: 'Superhero',
+			date: '2022-06-15T16:50:23.836Z',
+			__v: 0,
+		},
+		{
+			_id: '62aa0dcf331195387210018d',
+			user: '62a84bdc8097b4c47d521962',
+			title: 'DC Super',
+			description: 'Superheroes of DC universe',
+			tag: 'Superhero',
+			date: '2022-06-15T16:50:23.836Z',
+			__v: 0,
+		},
 	];
 	const [notes, setNotes] = useState(notesInitial);
 	return <NoteContext.Provider value={{ notes, setNotes }}>{props.children}</NoteContext.Provider>;
