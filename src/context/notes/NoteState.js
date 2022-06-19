9e1422bb7d338aa38f035c3b3ab40d024e25e57e
Imbeshat <md.imbeshat@gmail.com>
2022-06-19 19:17:14 +0530

    Adding fetch notes functionality

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,57 +2,40 @@ import { useState } from 'react';
 import NoteContext from './noteContext';
 
 const NoteState = (props) => {
-	const notesInitial = [
-		{
-			_id: '62a9772a76539aa43de8dabc0',
-			user: '62a84bdc8097b4c47d521962',
-			title: 'Marvels',
-			description: 'Superheroes of marvel universe',
-			tag: 'Superhero',
-			date: '2022-06-15T06:07:38.869Z',
-			__v: 0,
-		},
-		{
-			_id: '62aa0dcf33119w5387210018d',
-			user: '62a84bdc8097b4c47d521962',
-			title: 'DC Super',
-			description: 'Superheroes of DC universe',
-			tag: 'Superhero',
-			date: '2022-06-15T16:50:23.836Z',
-			__v: 0,
-		},
-		{
-			_id: '62aa0dcf3311s95387210018d',
-			user: '62a84bdc8097b4c47d521962',
-			title: 'DC Super',
-			description: 'Superheroes of DC universe',
-			tag: 'Superhero',
-			date: '2022-06-15T16:50:23.836Z',
-			__v: 0,
-		},
-		{
-			_id: '62aa0dcf3311953f87210018d',
-			user: '62a84bdc8097b4c47d521962',
-			title: 'DC Super',
-			description: 'Superheroes of DC universe',
-			tag: 'Superhero',
-			date: '2022-06-15T16:50:23.836Z',
-			__v: 0,
-		},
-		{
-			_id: '62aa0dcf3311953c87210018d',
-			user: '62a84bdc8097b4c47d521962',
-			title: 'DC Super',
-			description: 'Superheroes of DC universe',
-			tag: 'Superhero',
-			date: '2022-06-15T16:50:23.836Z',
-			__v: 0,
-		},
-	];
+	const host = 'http://localhost:5000';
+	const notesInitial = [];
 	const [notes, setNotes] = useState(notesInitial);
 
+	//   Get all Note
+	const getNotes = async () => {
+		//  API call
+		const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				'auth-token':
+					'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJhODRiZGM4MDk3YjRjNDdkNTIxOTYyIn0sImlhdCI6MTY1NTIwMDMzOX0.T1_wh7Rt823jBjNJguMo9dzajksum6FwjnkXvAHa8BQ',
+			},
+		});
+		const json = await response.json();
+		console.log(json);
+		setNotes(json);
+	};
+
 	//   Add a Note
-	const addNote = (title, description, tag) => {
+	const addNote = async (title, description, tag) => {
+		//  API call
+		const response = await fetch(`${host}/api/notes/addnote`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'auth-token':
+					'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJhODRiZGM4MDk3YjRjNDdkNTIxOTYyIn0sImlhdCI6MTY1NTIwMDMzOX0.T1_wh7Rt823jBjNJguMo9dzajksum6FwjnkXvAHa8BQ',
+			},
+			body: JSON.stringify({ title, description, tag }),
+		});
+		// const json = response.json();
+
 		console.log('Adding a new note');
 		const note = {
 			_id: '62aa0dcf3311953c87210018d',
@@ -75,10 +58,34 @@ const NoteState = (props) => {
 	};
 
 	//  Edit a Note
-	const editNote = () => {};
+	const editNote = async (id, title, description, tag) => {
+		//  API call
+		const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'auth-token':
+					'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJhODRiZGM4MDk3YjRjNDdkNTIxOTYyIn0sImlhdCI6MTY1NTIwMDMzOX0.T1_wh7Rt823jBjNJguMo9dzajksum6FwjnkXvAHa8BQ',
+			},
+			body: JSON.stringify(title, description, tag),
+		});
+		const json = response.json();
+
+		//  Login to edit in client
+		for (let index = 0; index < notes.length; index++) {
+			const element = notes[index];
+			if (element._id === id) {
+				element.title = title;
+				element.description = description;
+				element.tag = tag;
+			}
+		}
+	};
 
 	return (
-		<NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>{props.children}</NoteContext.Provider>
+		<NoteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>
+			{props.children}
+		</NoteContext.Provider>
 	);
 };
 
