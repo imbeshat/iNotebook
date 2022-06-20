9dc76b34e7e043ebec60e7d29cf0e8ea4aa7808a
Imbeshat <md.imbeshat@gmail.com>
2022-06-20 20:19:41 +0530

    Code modified for login component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Home } from './components/Home';
 import { About } from './components/About';
 import NoteState from './context/notes/NoteState';
 import { Alert } from './components/Alert';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
 	return (
@@ -17,7 +19,8 @@ function App() {
 						<Routes>
 							<Route path='/' exact element={<Home />} />
 							<Route path='/about' exact element={<About />} />
-							{/* <Route path='/second' exact element={<Second />} /> */}
+							<Route path='/login' exact element={<Login />} />
+							<Route path='/signup' exact element={<Signup />} />
 						</Routes>
 					</div>
 				</Router>
