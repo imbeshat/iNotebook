817e6c590009343028a37b5efb92128e8b53dd93
Imbeshat <md.imbeshat@gmail.com>
2022-06-20 20:17:41 +0530

    Modified code to authenticate user when users tries to login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -44,6 +44,7 @@ router.post(
 				},
 			};
 			const authToken = jwt.sign(data, JWT_SECRET);
+
 			res.json({ authToken });
 			// res.json(user);
 		} catch (error) {
@@ -58,6 +59,7 @@ router.post(
 	'/login',
 	[body('email', 'Enter a valid email').isEmail(), body('password', 'Password lcan not br blank').exists()],
 	async (req, res) => {
+		let success = false;
 		//  If there are errors, return bad request and the errors
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
@@ -70,11 +72,13 @@ router.post(
 			// Check wheter the user with this email exists already
 			let user = await User.findOne({ email });
 			if (!user) {
-				return res.status(400).json({ error: 'Incorrect Credentials' });
+				success = false;
+				return res.status(400).json({ success, error: 'Incorrect Credentials' });
 			}
 			const passwordCompare = await bcrypt.compare(password, user.password);
 			if (!passwordCompare) {
-				return res.status(400).json({ error: 'Incorrect Credentials' });
+				success = false;
+				return res.status(400).json({ success, error: 'Incorrect Credentials' });
 			}
 
 			const data = {
@@ -83,7 +87,8 @@ router.post(
 				},
 			};
 			const authToken = jwt.sign(data, JWT_SECRET);
-			res.json({ authToken });
+			success = true;
+			res.json({ success, authToken });
 		} catch (error) {
 			console.error(error.message);
 			res.status(500).send('Internal Server Error');
